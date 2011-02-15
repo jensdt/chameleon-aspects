7c31bccbf4f996475c1be27e088eca2fcd345a3c
Jens De Temmerman <jens.dt+github@gmail.com>
2011-02-15 11:35:49 +0100

    Implementing pointcut parameters

--- src/chameleon/aspects/pointcut/PointcutReference.java
+++ src/chameleon/aspects/pointcut/PointcutReference.java
@@ -1,34 +1,21 @@
 package chameleon.aspects.pointcut;
 
-import java.lang.ref.SoftReference;
 import java.util.List;
 
-import org.rejuse.association.OrderedMultiAssociation;
-import org.rejuse.java.collections.Visitor;
-
-import chameleon.aspects.pointcut.expression.PointcutExpressionParameter;
-import chameleon.core.Config;
 import chameleon.core.declaration.Declaration;
-import chameleon.core.declaration.DeclarationContainer;
-import chameleon.core.declaration.DeclarationWithParametersHeader;
-import chameleon.core.declaration.Signature;
-import chameleon.core.declaration.SimpleNameDeclarationWithParametersSignature;
-import chameleon.core.element.Element;
+import chameleon.core.expression.Expression;
+import chameleon.core.expression.InvocationTarget;
+import chameleon.core.expression.NamedTargetExpression;
 import chameleon.core.lookup.DeclarationSelector;
-import chameleon.core.lookup.DeclaratorSelector;
 import chameleon.core.lookup.LookupException;
 import chameleon.core.lookup.SimpleNameCrossReferenceWithArgumentsSelector;
-import chameleon.core.lookup.TwoPhaseDeclarationSelector;
-import chameleon.core.member.MoreSpecificTypesOrder;
-import chameleon.core.namespace.NamespaceElementImpl;
 import chameleon.core.reference.CrossReference;
 import chameleon.core.reference.CrossReferenceWithArguments;
-import chameleon.core.relation.WeakPartialOrder;
 import chameleon.core.validation.Valid;
 import chameleon.core.validation.VerificationResult;
 import chameleon.core.variable.FormalParameter;
 import chameleon.oo.type.Type;
-import chameleon.support.member.simplename.SimpleNameMethodInvocation;
+import chameleon.oo.type.generics.ActualTypeArgument;
 
 public class PointcutReference<E extends PointcutReference<E>> extends
 		CrossReferenceWithArguments<E> implements CrossReference<E, Pointcut> {
@@ -87,6 +74,46 @@ public class PointcutReference<E extends PointcutReference<E>> extends
 	public VerificationResult verifySelf() {
 		return Valid.create();
 	}
+	
+
+	public boolean hasParameter(FormalParameter fp) {
+		return indexOfParameter(fp) != -1;
+	}
+
+	public int indexOfParameter(FormalParameter fp) {
+		int index = 0;
+		
+		for (Expression param : getActualParameters()) {
+			if (!(param instanceof NamedTargetExpression))
+				continue;
+			
+			if (((NamedTargetExpression) param).name().equals(fp.getName()))
+				return index;
+			
+			index++;
+		}
+			
+		
+		return -1;
+	}
+
+	@Override
+	public E clone() {
+		InvocationTarget target = null;
+		if (getTarget() != null) {
+			target = getTarget().clone();
+		}
+		final E result = (E) new PointcutReference<E>(name());
+		result.setTarget(target);
+		for (Expression element : getActualParameters()) {
+			result.addArgument(element.clone());
+		}
+		for (ActualTypeArgument arg : typeArguments()) {
+			result.addArgument(arg.clone());
+		}
+		return result;
+	}
+
 
 
 	
